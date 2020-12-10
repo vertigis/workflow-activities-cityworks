008316c9e1886886edef313e2be189e73200eef8
Ryan Cooney <rcooney@latitudegeo.com>
2020-12-09 16:44:01 -0800

    feat: Add Q&A activities

---
+++ src/activities/ams/GetInspectionQuestions.ts
@@ -0,0 +1,55 @@
+import type { IActivityHandler } from "@geocortex/workflow/runtime/IActivityHandler";
+import { InspectionService } from "cw-sdk/api-service";
+import { IApiService } from "cw-sdk/api-service/i-api-service";
+import { InspQuestion } from "cw-sdk/core/types";
+import { checkResponse } from "../utils";
+
+/** An interface that defines the inputs of the activity. */
+export interface GetInspectionQuestionsInputs {
+    /**
+     * @displayName API Service
+     * @description The Cityworks API Service.
+     * @required
+     */
+    service: IApiService;
+    /**
+     * @displayName Inspection IDs
+     * @description The ID or IDs of the inspections to get the questions from.
+     * @required
+     */
+    inspectionIds: number | number[];
+}
+
+/** An interface that defines the outputs of the activity. */
+export interface GetInspectionQuestionsOutputs {
+    /**
+     * @description The list of inspection questions.
+     */
+    result?: InspQuestion[];
+}
+
+/**
+ * @category Cityworks - AMS
+ * @description Gets the questions for a Cityworks inspection.
+ */
+export class GetInspectionQuestions implements IActivityHandler {
+    async execute(inputs: GetInspectionQuestionsInputs): Promise<GetInspectionQuestionsOutputs> {
+        if (!inputs.service) {
+            throw new Error("service is required");
+        }
+        if (!inputs.inspectionIds) {
+            throw new Error("inspectionIds is required");
+        }
+
+        const service = new InspectionService(inputs.service);
+
+        const response = await service.Questions({
+            InspectionId: typeof inputs.inspectionIds === "number" ? inputs.inspectionIds : undefined as any,
+            InspectionIds: typeof inputs.inspectionIds !== "number" ? inputs.inspectionIds : undefined as any
+        });
+        checkResponse(response);
+        return {
+            result: response.Value,
+        };
+    }
+}
---
+++ src/activities/ams/GetInspectionTemplatesQuestionsAndAnswers.ts
@@ -0,0 +1,54 @@
+import type { IActivityHandler } from "@geocortex/workflow/runtime/IActivityHandler";
+import { InspectionTemplateService } from "cw-sdk/api-service";
+import { IApiService } from "cw-sdk/api-service/i-api-service";
+import { InspTemplateQA } from "cw-sdk/core/types";
+import { checkResponse } from "../utils";
+
+/** An interface that defines the inputs of the activity. */
+export interface GetInspectionTemplatesQuestionsAndAnswersInputs {
+    /**
+     * @displayName API Service
+     * @description The Cityworks API Service.
+     * @required
+     */
+    service: IApiService;
+    /**
+     * @description The ID or IDs of the inspection templates to get the questions and answers from.
+     * @required
+     */
+    templateIds?: number | number[];
+}
+
+/** An interface that defines the outputs of the activity. */
+export interface GetInspectionTemplatesQuestionsAndAnswersOutputs {
+    /**
+     * @description The list of questions and answers.
+     */
+    result?: InspTemplateQA[];
+}
+
+/**
+ * @defaultName cwInspectionTemplatesQA
+ * @category Cityworks - AMS
+ * @description Gets the questions and answers for a Cityworks inspection template.
+ */
+export class GetInspectionTemplatesQuestionsAndAnswers implements IActivityHandler {
+    async execute(inputs: GetInspectionTemplatesQuestionsAndAnswersInputs): Promise<GetInspectionTemplatesQuestionsAndAnswersOutputs> {
+        if (!inputs.service) {
+            throw new Error("service is required");
+        }
+        if (!inputs.templateIds) {
+            throw new Error("templateIds is required");
+        }
+
+        const service = new InspectionTemplateService(inputs.service);
+        const response = await service.QA({
+            InspTemplateIds: typeof inputs.templateIds === "number" ? [inputs.templateIds] : inputs.templateIds,
+        });
+        checkResponse(response);
+
+        return {
+            result: response.Value,
+        };
+    }
+}
